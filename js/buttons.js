be9ad95023bea5e9ecf7a6d99b75ceba1773c656
NotEvanH <158800060+NotEvanH@users.noreply.github.com>
2025-05-16 20:29:42 +0800

    Update buttons.js

--- js/buttons.js
+++ js/buttons.js
@@ -4,8 +4,14 @@ function click() {
   location.href = "https://javelinwebdesign.com/contact/";
 }
 
+function clickTwo() {
+  location.href = "https://javelinwebdesign.com/about/";
+}
+
 buttons.forEach((button) => {
   if (button.innerHTML.includes("Get") || button.innerHTML.includes("Contact")) {
     button.addEventListener('click', click);
+  } else if (button.innerHTML.includes("About")) {
+    button.addEventListener('click', clickTwo);
   }
 });
