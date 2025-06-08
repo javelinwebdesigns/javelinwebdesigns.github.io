591c3b35e68b2109cb4d0cebeb118e137884a7b1
NotEvanH <158800060+NotEvanH@users.noreply.github.com>
2025-06-08 13:41:18 +0800

    Update load.js

--- js/load.js
+++ js/load.js
@@ -1,4 +1,7 @@
-var preloaderContainer = document.querySelector(".preloader-container");
+const preloaderContainer = document.querySelector(".preloader-container");
+const container = document.querySelector(".container");
+
+container.style.display = "none";
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -7,9 +10,9 @@ function sleep(ms) {
 async function hidePreloader() {
     await sleep(4000);
     preloaderContainer.style.opacity = "0";
+    container.style.display = "block";
     await sleep(1000);
     preloaderContainer.style.display = "none";
 }
 
 hidePreloader();
-
